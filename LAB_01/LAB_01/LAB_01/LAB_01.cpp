3c88df205a52328b068cdca485c7bdf0ad358689
Piotrr0 <piotrr.nawalany@gmail.com>
2024-10-10 20:35:40 +0200

    Update LAB_01.cpp

--- LAB_01/LAB_01/LAB_01/LAB_01.cpp
+++ LAB_01/LAB_01/LAB_01/LAB_01.cpp
@@ -65,6 +65,24 @@ std::vector<std::pair<int, int>> allAmicable(int n)
 	return res;
 }
 
+int stepsHelper(int n, int currect, int prevStep, int min)
+{
+	if (currect > n) return INT_MAX;
+	if (currect == n) return min;
+
+	int stepMinus = (prevStep > 1) ? stepsHelper(n, currect + prevStep - 1, prevStep - 1, min + 1) : INT_MAX;
+	int stepSame = stepsHelper(n, currect + prevStep, prevStep, min + 1);
+	int stepPlus =stepsHelper(n, currect + prevStep + 1, prevStep + 1, min + 1);
+
+	return std::min({ stepMinus, stepSame, stepPlus });
+}
+
+int steps(int n) 
+{
+	if (n <= 0) return 0;
+	return stepsHelper(n, 1, 1, 1);
+}
+
 int main()
 {
 	std::cout << "suma dzelnikow 15 = " << divSum(15) << std::endl;
@@ -81,4 +99,5 @@ int main()
 	std::cout << std::endl;
 
 	std::cout << "Liczby 220 i 284 " << areAmicable(220, 284) << std::endl;
+	std::cout << "Liczba krokow:  " << steps(7) << std::endl;
 }
