157ccc9cc25e14273b3d1d7c2037fbc743a97522
Piotrr0 <piotrr.nawalany@gmail.com>
2025-01-10 12:40:26 +0100

    Update sort

--- LAB_08/LAB_08/LAB_08.cpp
+++ LAB_08/LAB_08/LAB_08.cpp
@@ -1,41 +1,33 @@
 ﻿#include <iostream>
 #include <ctime>
-#include <cstdlib>
-#include <random>
+#include "unordered_map"
 
 void InsertionSort(int* arr, int n)
 {
-	for (int i = 1; i < n; ++i)
+	for (int i = 1; i < n; i++)
 	{
-		int key = arr[i];
-		int j = i - 1;
-
-		while (j >= 0 && arr[j] > key)
+		int j = i;
+		while (j >= 0 && arr[j] < arr[j - 1])
 		{
-			arr[j + 1] = arr[j];
-			j = j - 1;
+			std::swap(arr[j], arr[j-1]);
+			j--;
 		}
-		arr[j + 1] = key;
 	}
 }
 
 void SelectionSort(int* arr, int n)
 {
-	for (int i = 0; i < n - 1; i++)
+	for (int i = 0; i < n; i++)
 	{
-		int min_idx = i;
-
-		for (int j = i + 1; j < n; j++)
+		int minIndex = i;
+		for (int j = i+1; j < n; j++)
 		{
-			if (arr[j] < arr[min_idx])
+			if (arr[j] < arr[minIndex])
 			{
-				min_idx = j;
+				minIndex = j; 
 			}
 		}
-
-		int temp = arr[i];
-		arr[i] = arr[min_idx];
-		arr[min_idx] = temp;
+		std::swap(arr[i], arr[minIndex]);
 	}
 }
 
@@ -47,75 +39,44 @@ void BubbleSort(int* arr, int n)
 		{
 			if (arr[j] > arr[j + 1])
 			{
-				int temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
+				std::swap(arr[j], arr[j + 1]);
 			}
 		}
 	}
 }
 
-int Partition(int* arr, int low, int high)
+int Partition(int* arr, int left, int right)
 {
-	int pivot = arr[high];
-	int i = low - 1;
-
-	for (int j = low; j <= high - 1; j++)
+	int pivot = arr[right];
+	int i = left - 1;
+	for (int j = left; j < right; j++)
 	{
 		if (arr[j] < pivot)
 		{
 			i++;
-			int temp = arr[j];
-			arr[j] = arr[i];
-			arr[i] = temp;
+			std::swap(arr[i], arr[j]);
 		}
 	}
-
-	int temp = arr[i + 1];
-	arr[i + 1] = arr[high];
-	arr[high] = temp;
-
-	return i + 1;
+	i++;
+	std::swap(arr[i], arr[right]);
+	return i;
 }
 
-void QuickSortHelper(int* arr, int low, int high)
+void QuickSort(int* arr, int left, int right)
 {
-	if (low < high)
-	{
-		int pivot = Partition(arr, low, high);
-		QuickSortHelper(arr, low, pivot - 1);
-		QuickSortHelper(arr, pivot + 1, high);
-	}
-}
-
-void QuickSort(int* arr, int n)
-{
-	QuickSortHelper(arr, 0, n - 1);
+	if (right <= left) return;
+	int pivot = Partition(arr, left, right);
+	QuickSort(arr, left, pivot - 1);
+	QuickSort(arr, pivot + 1, right);
 }
 
 int main()
 {
-	int n = 10000;
-	int* arr = new int[n];
-
-	std::random_device dev;
-	std::mt19937 rng(dev());
-	std::uniform_int_distribution<std::mt19937::result_type> dist6(1, 100);
-
-	for (int i = 0; i < n; i++)
+	int* arr = new int[10] {2, 7, 1, 5, 2, 8, 9, 0, 2, 11};
+	BubbleSort(arr, 10);
+	for (int i = 0; i < 10; i++)
 	{
-		arr[i] = dist6(rng);
+		std::cout << arr[i] << std::endl;
 	}
 
-	clock_t start = clock();
-	QuickSort(arr, n);
-	clock_t stop = clock();
-	double resultTime = (double)(stop - start) / CLOCKS_PER_SEC;
-	std::cout << resultTime << std::endl;
-
-	for (int i = 0; i < n; i++)
-	{
-		//std::cout << arr[i] << std::endl;
-	}
-	delete[] arr;
 }
