2508d0a62acad5f7ead71e69e164fab35bb34a8a
Piotrr0 <piotrr.nawalany@gmail.com>
2025-01-13 18:20:11 +0100

    Update sort

--- LAB_08/LAB_08/LAB_08.cpp
+++ LAB_08/LAB_08/LAB_08.cpp
@@ -70,10 +70,82 @@ void QuickSort(int* arr, int left, int right)
 	QuickSort(arr, pivot + 1, right);
 }
 
+void CountSort(int* arr, int n)
+{
+	int maxVal = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] > maxVal) 
+		{
+			maxVal = arr[i];
+		}
+	}
+
+	int* countArr = new int[maxVal + 1]();
+	for (int i = 0; i < n; i++)
+	{
+		countArr[arr[i]]++;
+	}
+
+	for (int i = 1; i < maxVal + 1; i++) 
+	{
+		countArr[i] += countArr[i - 1];
+	}
+
+	int* outputArr = new int[n];
+	for (int i = n - 1; i >= 0; i--) 
+	{
+		outputArr[countArr[arr[i]] - 1] = arr[i];
+		countArr[arr[i]]--;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = outputArr[i];
+	}
+
+	delete[] countArr;
+	delete[] outputArr;
+}
+
+void RadixBinSort(int* arr, int n)
+{
+	std::unordered_map<int, std::vector<int>> bin;
+	int maxVal = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] > maxVal)
+		{
+			maxVal = arr[i];
+		}
+	}
+
+	int exp = 1;
+	while (maxVal / exp > 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			int binIndex = (arr[i] / exp) % 10;
+			bin[binIndex].push_back(arr[i]);
+		}
+
+		int index = 0;
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < bin[i].size(); j++)
+			{
+				arr[index++] = bin[i][j];
+			}
+			bin[i].clear();
+		}
+		exp *= 10;
+	}
+}
+
 int main()
 {
 	int* arr = new int[10] {2, 7, 1, 5, 2, 8, 9, 0, 2, 11};
-	BubbleSort(arr, 10);
+	RadixBinSort(arr, 10);
 	for (int i = 0; i < 10; i++)
 	{
 		std::cout << arr[i] << std::endl;
